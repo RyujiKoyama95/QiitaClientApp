d160cc26dcbbd09702b55e2509c0a092af8349f5
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-09 10:22:51 +0900

    Add navigation process search screen to detail screen

--- app/src/main/java/com/example/qootaclient/MainActivity.kt
+++ app/src/main/java/com/example/qootaclient/MainActivity.kt
@@ -10,7 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.qootaclient.ui.theme.QootaClientTheme
+import com.example.qootaclient.view.detail.DetailScreen
+import com.example.qootaclient.view.search.SearchScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +29,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val navController = rememberNavController()
+                    MainNavHost(navController = navController)
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MainNavHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "search") {
+        composable("search") { SearchScreen(navController) }
+        composable("detail") { DetailScreen(navController) }
     }
 }
