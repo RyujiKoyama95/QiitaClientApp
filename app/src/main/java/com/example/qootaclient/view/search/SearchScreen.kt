0eaf52e8456d01a201eaf1e20216a3e710557b64
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-11 23:57:11 +0900

    Add call SearchView process to SearchScreen

--- app/src/main/java/com/example/qootaclient/view/search/SearchScreen.kt
+++ app/src/main/java/com/example/qootaclient/view/search/SearchScreen.kt
@@ -7,7 +7,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.navigation.NavController
+import com.example.qootaclient.view.component.SearchView
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -15,6 +19,10 @@ import androidx.navigation.NavController
 fun SearchScreen(navController: NavController) {
     Scaffold {
         Column {
+            val textFieldState = remember { mutableStateOf(TextFieldValue("")) }
+            SearchView(textFieldState = textFieldState) {
+                // Todo: callback
+            }
             Text(text = "検索画面")
             Button(onClick = { navController.navigate("detail") }) {
                 Text(text = "詳細画面へ")
