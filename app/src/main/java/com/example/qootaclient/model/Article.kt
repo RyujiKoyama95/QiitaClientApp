1a7862413cfb7aa9637ec5f9643876f7e95b5030
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-13 23:36:55 +0900

    Add Article model

---
+++ app/src/main/java/com/example/qootaclient/model/Article.kt
@@ -0,0 +1,15 @@
+package com.example.qootaclient.model
+
+import com.squareup.moshi.Json
+
+data class Article(
+    val url: String,
+    val title: String,
+    val user: User
+) {
+    data class User(
+        val id: String,
+        @Json(name = "profile_image_url")
+        val profileImageUrl: String
+    )
+}
