d819cd3e4b2b0be60244385c70cfb323d4a6925a
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-14 20:51:17 +0900

    Add WebView

--- app/src/main/java/com/example/qootaclient/view/detail/DetailScreen.kt
+++ app/src/main/java/com/example/qootaclient/view/detail/DetailScreen.kt
@@ -3,15 +3,14 @@
 package com.example.qootaclient.view.detail
 
 import android.annotation.SuppressLint
+import android.webkit.WebView
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import retrofit2.http.Url
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun DetailScreen() {
-    Scaffold {
-        Text(text = "詳細画面")
-    }
+fun DetailScreen(url: Url) {
+    val state = rememberWebViewState(url)
+    WebView(state)
 }
