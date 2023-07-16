70874a2af675897e73361711fa17f112fa5c34e8
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-16 18:59:54 +0900

    Add creating database and Dao API

--- app/src/main/java/com/example/qootaclient/data/Room.kt
+++ app/src/main/java/com/example/qootaclient/data/Room.kt
@@ -1,4 +1,39 @@
 package com.example.qootaclient.data
 
-interface Room {
+import android.content.Context
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Dao
+interface Dao {
+    @Query("SELECT * FROM Article")
+    suspend fun getArticles(): List<Article>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(articles: List<Article>)
+}
+
+@Database(entities = [Article::class], version = 1, exportSchema = false)
+abstract class ArticleDatabase: RoomDatabase() {
+    abstract fun dao(): Dao
+
+    companion object {
+        private var INSTANCE: ArticleDatabase? = null
+
+        fun getDatabase(context: Context): ArticleDatabase {
+            INSTANCE ?: synchronized(this) {
+                INSTANCE = Room.databaseBuilder(
+                    context,
+                    ArticleDatabase::class.java,
+                    "article_database"
+                ).fallbackToDestructiveMigration().build()
+            }
+            return INSTANCE as ArticleDatabase
+        }
+    }
 }
