0607bba149e4e088a25ef2d6c1b96ef9478ef783
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-16 19:19:50 +0900

    Add viewmodel

--- app/src/main/java/com/example/qootaclient/view/search/SearchViewModel.kt
+++ app/src/main/java/com/example/qootaclient/view/search/SearchViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.qootaclient.view.search
 
-class SearchViewModel {
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.qootaclient.data.Article
+import com.example.qootaclient.data.ArticleDatabase
+import com.example.qootaclient.repository.ArticleRepository
+import kotlinx.coroutines.launch
+
+class SearchViewModel(private val application: Application): ViewModel() {
+    private val dao = ArticleDatabase.getDatabase(application).dao()
+    private val repo = ArticleRepository(dao)
+    private var _articles = MutableLiveData<List<Article>>()
+    val article: LiveData<List<Article>> = _articles
+
+    fun refreshArticles(query: String) {
+        viewModelScope.launch {
+            repo.refreshArticles(query)
+            val result = repo.getArticles()
+            _articles.postValue(result)
+        }
+    }
 }
