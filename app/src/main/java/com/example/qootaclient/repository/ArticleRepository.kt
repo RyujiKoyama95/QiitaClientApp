9c0e62752e4f788245fe177e880c5b06aae4af3d
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-16 19:04:11 +0900

    Add method that store data retrieved from network

--- app/src/main/java/com/example/qootaclient/repository/ArticleRepository.kt
+++ app/src/main/java/com/example/qootaclient/repository/ArticleRepository.kt
@@ -1,6 +1,9 @@
 package com.example.qootaclient.repository
 
+import com.example.qootaclient.data.Article
+import com.example.qootaclient.data.Dao
 import com.example.qootaclient.model.QiitaApiArticle
+import com.example.qootaclient.model.asDatabaseModel
 import com.example.qootaclient.network.QiitaService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -8,7 +11,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-class ArticleRepository {
+class ArticleRepository(private val dao: Dao) {
     companion object {
         val qiitaService = Retrofit.Builder()
             .baseUrl(QiitaService.BASE_URL)
@@ -24,4 +27,16 @@ class ArticleRepository {
 
     suspend fun searchArticle(query: String): List<QiitaApiArticle>? =
         qiitaService.searchArticle(query).body()
+
+    /**
+     * API通信で取得したデータをローカルデータベースで保持する
+     */
+    suspend fun refreshArticles(query: String) {
+        val videos = qiitaService.searchArticle(query).body()
+        if (videos != null) {
+            dao.insertAll(videos.asDatabaseModel())
+        }
+    }
+
+    suspend fun getArticles(): List<Article> = dao.getArticles()
 }
